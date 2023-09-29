657aeb2263fd50385f787d7aa7a47161a663b2f7
Ika1009 <ilija.doncic@yahoo.com>
2023-09-29 14:02:53 +0200

    changed signup php to ake more variables

--- DB_APIs/signup.php
+++ DB_APIs/signup.php
@@ -1,5 +1,4 @@
 <?php
-
 require 'db_conn.php';  // Include the database connection
 
 header('Content-Type: application/json');
@@ -7,6 +6,8 @@ header('Content-Type: application/json');
 $email = $_POST['email'];
 $password = $_POST['password'];
 $fullName = $_POST['full_name'];
+$location = $_POST['location']; // New field for location
+$jobPreference = $_POST['job_preference']; // New field for job_preference
 
 // Split the full name into first and last name
 $names = explode(' ', $fullName);
@@ -14,7 +15,7 @@ $firstName = $names[0];
 $lastName = isset($names[1]) ? $names[1] : '';
 
 // Check if data is provided
-if (isset($email, $password, $firstName, $lastName)) {
+if (isset($email, $password, $firstName, $lastName, $location, $jobPreference)) {
     // First, check if the email already exists
     $stmt = $pdo->prepare('SELECT email FROM users WHERE email = ?');
     $stmt->execute([$email]);
@@ -26,15 +27,14 @@ if (isset($email, $password, $firstName, $lastName)) {
 
     // If email doesn't exist, proceed with registration
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-    $stmt = $pdo->prepare('INSERT INTO users (email, password, first_name, last_name) VALUES (?, ?, ?, ?)');
-    $stmt->execute([$email, $hashedPassword, $firstName, $lastName]);
+    $stmt = $pdo->prepare('INSERT INTO users (email, password, first_name, last_name, location, job_preference) VALUES (?, ?, ?, ?, ?, ?)');
+    $stmt->execute([$email, $hashedPassword, $firstName, $lastName, $location, $jobPreference]);
 
     echo json_encode(['status' => 'success', 'message' => 'User registered successfully']);
     exit;
 
 } else {
-    echo json_encode(['status' => 'error', 'message' => 'Email and password are required']);
+    echo json_encode(['status' => 'error', 'message' => 'Email, password, location, and job_preference are required']);
     exit;
 }
-
 ?>
