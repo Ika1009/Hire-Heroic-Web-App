675ae3d65b9f2dd4744702924b992a53d35860cb
Ilija Dončić <75073537+Ika1009@users.noreply.github.com>
2023-10-02 10:30:41 +0200

    Update signup.php debgging

--- DB_APIs/signup.php
+++ DB_APIs/signup.php
@@ -3,14 +3,22 @@ require 'db_conn.php';  // Include the database connection
 
 header('Content-Type: application/json');
 
+// Log or print all POST variables
+error_log("POST variables: " . json_encode($_POST));
+
 $email = isset($_POST['email']) ? $_POST['email'] : null;
 $password = isset($_POST['password']) ? $_POST['password'] : null;
 $fullName = isset($_POST['full_name']) ? $_POST['full_name'] : null;
 $location = isset($_POST['location']) ? $_POST['location'] : null;
 $jobPreference = isset($_POST['job_preference']) ? $_POST['job_preference'] : null;
 
-// Check if data is provided
-if (isset($email, $password, $fullName, $location, $jobPreference)) {
+$missing_vars = [];
+
+foreach(['email', 'password', 'fullName', 'location', 'jobPreference'] as $var) {
+    if (!isset($$var)) $missing_vars[] = $var;
+}
+
+if (empty($missing_vars)) {
 
     // Split the full name into first and last name
     $names = explode(' ', $fullName);
@@ -34,17 +42,13 @@ if (isset($email, $password, $fullName, $location, $jobPreference)) {
     echo json_encode(['status' => 'success', 'message' => 'User registered successfully']);
     exit;
 
-}else {
-    $missing_vars = [];
-    foreach(['email', 'password', 'fullName', 'location', 'jobPreference'] as $var) {
-        if (!isset($$var)) $missing_vars[] = $var;
-    }
+} else {
     echo json_encode([
         'status' => 'error', 
         'message' => 'Some variables are missing', 
-        'missing_vars' => $missing_vars
+        'missing_vars' => $missing_vars,
+        'debug_post_data' => $_POST  // Debugging line
     ]);
     exit;
 }
-
 ?>
