7f609e3eec1d47a5b02899598e5a407f02677036
Ika1009 <ilija.doncic@yahoo.com>
2023-10-10 11:47:37 +0200

    Changed Typo on pricing page

--- pricing.php
+++ pricing.php
@@ -77,7 +77,7 @@
         <div class="pt-8 pb-16 px-4 mx-auto max-w-screen-xl lg:px-6">
             <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
                 <h2 class="mb-4 text-2xl md:text-3xl tracking-tight font-extrabold text-gray-900 ">Designed for business teams like yours</h2>
-                <p class="mb-5 font-light text-gray-500 text-lg sm:text-xl ">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+                <p class="mb-5 font-light text-gray-500 text-lg sm:text-xl ">Here at Hireheroic we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
             </div>
             <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
                 <!-- Pricing Card -->
